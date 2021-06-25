b4d24d8da03691e7c078acc11b46edb781cdd074
Dominick Rose <Dom.Rose42@gmail.com>
2021-06-25 09:55:05 -0600

    Wrote registration service interface and tests

---
+++ src/test/java/com/ismadoro/services/RegistrationServiceTests.java
@@ -0,0 +1,66 @@
+package com.ismadoro.services;
+
+import com.ismadoro.daos.RegistrationDao;
+import com.ismadoro.entities.Registration;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistrationServiceTests {
+
+    final RegistrationDao registrationDao = Mockito.mock(RegistrationDao.class);
+    RegistrationService registrationService = null;
+
+    @BeforeMethod
+    void mockSetup() {
+        List<Registration> registrationList = new ArrayList<>();
+        registrationList.add(new Registration(1, 1, 1));
+        registrationList.add(new Registration(2, 1, 2));
+        registrationList.add(new Registration(4, 1, 3));
+        registrationList.add(new Registration(3, 2, 1));
+        registrationList.add(new Registration(5, 2, 2));
+
+        Mockito.when(registrationDao.getAllRegistrations()).thenReturn(registrationList);
+//        registrationService = new RegistrationServiceImpl(registrationDao)
+    }
+
+    @Test(priority = 1)
+    void testGetPlayer1Events() {
+        List<Integer> result = registrationService.getAllEventsForPlayer(1);
+        Assert.assertEquals(result.size(), 3);
+    }
+
+    @Test(priority = 1)
+    void testGetPlayer2Events() {
+        List<Integer> result = registrationService.getAllEventsForPlayer(2);
+        Assert.assertEquals(result.size(), 2);
+    }
+
+    @Test(priority = 1)
+    void testGetEmptyRegistrationEvents() {
+        List<Integer> result = registrationService.getAllEventsForPlayer(3);
+        Assert.assertEquals(result.size(), 0);
+    }
+
+    @Test(priority = 2)
+    void testGetEvent1Players() {
+        List<Integer> result = registrationService.getAllPlayersForEvent(1);
+        Assert.assertEquals(result.size(), 2);
+    }
+
+    @Test(priority = 2)
+    void testGetEvent3Players() {
+        List<Integer> result = registrationService.getAllPlayersForEvent(3);
+        Assert.assertEquals(result.size(), 1);
+    }
+
+    @Test(priority = 2)
+    void testGetEmptyEvent() {
+        List<Integer> results = registrationService.getAllPlayersForEvent(0);
+        Assert.assertEquals(results.size(), 0);
+    }
+}
