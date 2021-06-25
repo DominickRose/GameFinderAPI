5866b13d537192131047f163988b7019a54ee0d2
Dominick Rose <Dom.Rose42@gmail.com>
2021-06-25 09:35:26 -0600

    Finsihed writing Local Registration DAO.  Can't test until events are finished so I'm going to move onto services

--- src/test/java/com/ismadoro/daos/RegistrationDaoTests.java
+++ src/test/java/com/ismadoro/daos/RegistrationDaoTests.java
@@ -2,13 +2,18 @@ package com.ismadoro.daos;
 
 import com.ismadoro.entities.Event;
 import com.ismadoro.entities.Player;
+import com.ismadoro.entities.Registration;
+import com.ismadoro.exceptions.ResourceNotFound;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 public class RegistrationDaoTests {
-    private static RegistrationDao registrationDao = null;
+    private static RegistrationDao registrationDao = new RegistrationDaoLocal();
     private static PlayerDao playerDao = new PlayerDaoLocal();
     private static EventDao eventDao = null; //Initialize once finished
 
@@ -18,6 +23,9 @@ public class RegistrationDaoTests {
     //Once Events are finished, initialize these
     private static final Event testEvent1 = null;
     private static final Event testEvent2 = null;
+
+    private final Registration testRegistration = new Registration(0, testPlayer1.getPlayerId(), 1); //Edit this to be testEvent1 id once that's completed
+    private final Registration testRegistration2 = new Registration(0, testPlayer2.getPlayerId(), 2); //Edit this to be testEvent1 id once that's completed
 
     @BeforeClass
     void setup() {
@@ -37,5 +45,77 @@ public class RegistrationDaoTests {
 //        eventDao.deletePlayer(testEvent2.getEventId());
     }
 
-    
+    @Test(priority = 1)
+    void testAddRegistration1() {
+        Registration result = registrationDao.addRegistration(testRegistration);
+        Assert.assertNotEquals(result.getRegistrationId(), 0);
+    }
+
+    @Test(priority = 2)
+    void testAddRegistration2() {
+        Registration result = registrationDao.addRegistration(testRegistration2);
+        Assert.assertNotEquals(result.getRegistrationId(), testRegistration.getRegistrationId());
+    }
+
+    @Test(priority = 3)
+    void testGetAllRegistrations(){
+        List<Registration> registrationList = registrationDao.getAllRegistrations();
+        Assert.assertTrue(registrationList.size() >= 2);
+    }
+
+    @Test(priority = 4)
+    void testGetSingleRegistration() {
+        Registration result = registrationDao.getSingleRegistation(testRegistration.getRegistrationId());
+        Assert.assertEquals(result.getRegistrationId(), testRegistration.getRegistrationId());
+    }
+
+    @Test(priority = 4)
+    void testGetSingleRegistration2() {
+        Registration result = registrationDao.getSingleRegistation(testRegistration2.getRegistrationId());
+        Assert.assertEquals(result.getRegistrationId(), testRegistration2.getRegistrationId());
+    }
+
+    @Test(priority = 4)
+    void testGetInvalidRegistration() {
+        try {
+            registrationDao.getSingleRegistation(0);
+            Assert.fail();
+        } catch (ResourceNotFound resourceNotFound) {
+            //Succeed
+        }
+    }
+
+    @Test(priority = 5)
+    void testUpdateSingleRegistration() {
+        testRegistration.setPlayerId(testPlayer2.getPlayerId());
+        Registration result = registrationDao.updateRegistration(testRegistration);
+        Assert.assertEquals(result.getPlayerId(), testPlayer2.getPlayerId());
+    }
+
+    @Test(priority = 6)
+    void testUpdateInvalidRegistration() {
+        Registration invalid = new Registration(0, 0, 0);
+        try {
+            registrationDao.updateRegistration(invalid);
+            Assert.fail();
+        } catch (ResourceNotFound resourceNotFound) {
+            //Succeed
+        }
+    }
+
+    @Test(priority = 7)
+    void testDeleteRegistration() {
+        boolean result = registrationDao.deleteRegistration(testRegistration2.getRegistrationId());
+        Assert.assertTrue(result);
+    }
+
+    @Test(priority = 8)
+    void testInvalidDelete() {
+        try {
+            registrationDao.deleteRegistration(testRegistration2.getRegistrationId());
+            Assert.fail();
+        } catch (ResourceNotFound resourceNotFound) {
+            //Succeed
+        }
+    }
 }
