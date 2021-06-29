de3cfdce124aab1608b6225013baf8343e19e5a2
Dominick Rose <Dom.Rose42@gmail.com>
2021-06-29 15:53:50 -0600

    Added bio to player entity

--- src/test/java/com/ismadoro/daos/RegistrationDaoTests.java
+++ src/test/java/com/ismadoro/daos/RegistrationDaoTests.java
@@ -17,8 +17,8 @@ public class RegistrationDaoTests {
     private static PlayerDao playerDao = new PlayerDaoLocal();
     private static EventDao eventDao = new EventDaoLocal(); //Initialize once finished
 
-    private static final Player testPlayer1 = new Player(0, "Player", "One", UUID.randomUUID().toString(), "test", true, "a@email.com", "1234567890", "WA", "Spokane");;
-    private static final Player testPlayer2 = new Player(0, "Player", "Two", UUID.randomUUID().toString(), "test", true, "a@email.com", "1234567890", "WA", "Spokane");;
+    private static final Player testPlayer1 = new Player(0, "Player", "One", UUID.randomUUID().toString(), "test", true, "a@email.com", "1234567890", "WA", "Spokane", "");;
+    private static final Player testPlayer2 = new Player(0, "Player", "Two", UUID.randomUUID().toString(), "test", true, "a@email.com", "1234567890", "WA", "Spokane", "");;
 
     //Once Events are finished, initialize these
     private static Event testEvent1 = null;
